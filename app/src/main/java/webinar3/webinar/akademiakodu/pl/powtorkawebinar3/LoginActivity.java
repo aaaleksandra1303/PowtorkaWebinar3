0ba719bc9ba2e668cada3c078bfac08b824f0304
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-12 22:30:47 +0200

    sent text from MainActivity to LoginActivity after button click

--- app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/LoginActivity.java
+++ app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/LoginActivity.java
@@ -4,18 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends Activity {
+
+    @BindView(R.id.responseText)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         Intent i = getIntent();
-        if(i.hasExtra("hej"))
+        if(i.hasExtra("key"))
         {
-            String response = i.getExtras().getString("key");
+            String response = i.getStringExtra("key");
+            textView.setText(response);
         }
     }
 }
--- app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/MainActivity.java
+++ app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/MainActivity.java
@@ -4,10 +4,19 @@ package webinar3.webinar.akademiakodu.pl.powtorkawebinar3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+
+    @BindView(R.id.openActivity)
+    Button openActivity;
+    public String text = "Przekazuje informacje";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +24,16 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        String text = "Przekazuje informacje";
+
+
+
+    }
+
+    @OnClick(R.id.openActivity)
+    public void clicked(View v)
+    {
         Intent i = new Intent(this,LoginActivity.class);
-
         i.putExtra("key",text);
-
         startActivity(i);
-
     }
 }

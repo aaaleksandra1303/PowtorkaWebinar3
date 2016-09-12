4ea01b533e5f9f75ea959b3ed0c6a634f4ab1fba
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-12 21:51:06 +0200

    get values  from ActivityLogin

--- app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/LoginActivity.java
+++ app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/LoginActivity.java
@@ -1,13 +1,21 @@
 package webinar3.webinar.akademiakodu.pl.powtorkawebinar3;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent i = getIntent();
+        if(i.hasExtra("hej"))
+        {
+            String response = i.getExtras().getString("key");
+        }
     }
 }
--- app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/MainActivity.java
+++ app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/MainActivity.java
@@ -2,6 +2,7 @@ package webinar3.webinar.akademiakodu.pl.powtorkawebinar3;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
@@ -14,6 +15,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        String text = "Przekazuje informacje";
+        Intent i = new Intent(this,LoginActivity.class);
+
+        i.putExtra("key",text);
+
+        startActivity(i);
 
     }
 }

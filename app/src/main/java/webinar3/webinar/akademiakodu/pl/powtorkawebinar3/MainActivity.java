f88a2d177e58327ae884c223814baf310cacc65a
Aleksandra Malek <aleksandra1303@gmail.com>
2016-09-12 21:33:43 +0200

    add new LoginActivity

---
+++ app/src/main/java/webinar3/webinar/akademiakodu/pl/powtorkawebinar3/MainActivity.java
@@ -0,0 +1,19 @@
+package webinar3.webinar.akademiakodu.pl.powtorkawebinar3;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+}
